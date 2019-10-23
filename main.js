42485f672d4684459ea9635bbbeb6b729d6398e0
cabal82 <fabriziogallici@gmail.com>
2019-10-23 14:37:11 +0200

    richiesta cognome utente

---
+++ main.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il cognome, inseriscilo in un
+// array con altri cognomi e stampa la lista
+// ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il
+// nuovo utente si trova
+
+
+var unorderSurname = ['Vena', 'Siri', 'Biava', 'Grassi', 'Inglima', 'Neroni', 'Rispoli', 'Polino'];
+
+//chiedo il Cognome, lo inserisco nell'array e visualizzo
+var userSurname = prompt('Inserisci il tuo Cognome');
+unorderSurname.push(userSurname);
+console.log(unorderSurname);
